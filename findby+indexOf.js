893ef912d5eb08b2a7927666616b67df9f6bf08d
yangfaguo123 <1129681601@qq.com>
2017-04-10 18:57:32 +0800

    init commit

---
+++ findby+indexOf.js
@@ -0,0 +1,59 @@
+/**
+ * Created by faguo on 17-4-9.
+ */
+
+var products = [
+    {
+        name: 'BBBSCCS',
+        price: 121,
+        count: 12
+    },
+    {
+        name: 'BBBSABCCCS',
+        price: 121,
+        count: 12
+    }
+    ,
+    {
+        name: 'BBBSABADCCS',
+        price: 121,
+        count: 12
+    }
+    ,
+    {
+        name: 'BBBABCSCCS',
+        price: 121,
+        count: 12
+    }
+    ,
+    {
+        name: 'BBBABGH',
+        price: 121,
+        count: 12
+    }
+    ,
+    {
+        name: 'BBBABC',
+        price: 121,
+        count: 12
+    },
+    {
+        name: 'BBBABD',
+        price: 121,
+        count: 12
+    }
+];
+function findby(arr, match) {
+    for (var i = 0; i < arr.length; i++) {
+        if (match(arr[i])) {
+            console.log(arr[i]);
+        }
+
+    }
+    return undefined;
+}
+
+
+findby(products, function (product) {
+    return product.name.indexOf("ABC") >= 0;
+});
